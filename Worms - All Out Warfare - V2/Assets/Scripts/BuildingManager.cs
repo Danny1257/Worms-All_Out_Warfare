eda20bdbb45a84741538bf7a383a4405116c3c96
Daniel Hayes <dannyhayes57@hotmail.co.uk>
2015-03-15 20:47:52 +0000

    Worms V2
    Version 2.0 of worms game.

---
+++ Worms - All Out Warfare - V2/Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingManager : MonoBehaviour {
+
+	public GameObject[] buildings;
+	private BuildingPlacement buildingPlacement;
+
+	public LayerMask buildButtonMask, buildingButtonMask, fortButtonMask, towerButtonMask;
+	public bool buildMenuOpen;
+
+
+	public GameObject Build_Button, Build_Menu, Barracks_Button, Fort_Button, Tower_Button, Cross_Button;
+	private GameObject build_Menu_Clone, barracks_Button_Clone, fort_Button_Clone, tower_Button_Clone, cross_Button_Clone;
+	private Touch touch;
+
+	// Use this for initialization
+	void Start () {
+		buildingPlacement = GetComponent<BuildingPlacement>();
+		buildMenuOpen = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 m = Input.mousePosition;
+		m = new Vector3 (m.x, m.y + transform.position.y, transform.position.y);
+
+		Vector3 p = Camera.main.ScreenToWorldPoint (m);
+		Vector3 n = new Vector3 (0.0f, -0.667f, 0.0f);
+
+		if (Input.GetMouseButtonDown (0))
+		{
+			if (Build_Button.guiTexture.HitTest (Input.mousePosition))
+			{
+				if (!buildMenuOpen)
+				{
+					Debug.Log ("Hit BUild button");							
+					Debug.Log ("Hit the GUI build button");
+					build_Menu_Clone = (GameObject)Instantiate(Build_Menu);
+					barracks_Button_Clone = (GameObject)Instantiate(Barracks_Button);
+					fort_Button_Clone = (GameObject)Instantiate(Fort_Button);
+					tower_Button_Clone = (GameObject)Instantiate(Tower_Button);
+					cross_Button_Clone = (GameObject)Instantiate(Cross_Button);
+					buildMenuOpen = true;
+				}
+
+			}
+			else if (buildMenuOpen)
+			{
+				if (Cross_Button.guiTexture.HitTest (Input.mousePosition))
+				{
+					RemoveBuildMenu();
+					buildMenuOpen = false;
+					Debug.Log ("Hit Cross_Button");
+				}
+				else if (Barracks_Button.guiTexture.HitTest (Input.mousePosition)) {
+					RemoveBuildMenu ();
+					buildingPlacement.SetItem (buildings [0]);
+					Debug.Log ("hit barracks_button");
+				}
+				else if (Fort_Button.guiTexture.HitTest (Input.mousePosition)) {
+					RemoveBuildMenu ();
+					buildingPlacement.SetItem (buildings [1]);
+					Debug.Log ("hit fort_button");
+				}
+				else if (Tower_Button.guiTexture.HitTest (Input.mousePosition)) {
+					RemoveBuildMenu ();
+					buildingPlacement.SetItem (buildings [2]);
+					Debug.Log ("hit fort_button");
+				}
+				else if (Build_Menu.guiTexture.HitTest (Input.mousePosition)) {
+					Debug.Log ("Hit the menu");
+				}			
+			}
+		}
+	}
+
+	void OnGUI()
+	{
+
+	}
+
+	void RemoveBuildMenu()
+	{
+		if (buildMenuOpen) {
+			Destroy (cross_Button_Clone);
+			Destroy (build_Menu_Clone);
+			Destroy (barracks_Button_Clone);
+			Destroy (fort_Button_Clone);
+			Destroy (tower_Button_Clone);
+			buildMenuOpen = false;
+		}
+	}
+}
---
+++ Worms - All Out Warfare - V2/Assets/Scripts/BuildingPlacement.cs
@@ -0,0 +1,158 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingPlacement : MonoBehaviour {
+
+	private PlaceableBuilding placeAbleBuilding;
+	private Transform currentBuilding;
+	private bool hasPlaced;
+	public LayerMask buildingsMask;
+	public LayerMask MotionMask;
+	public LayerMask MenuMask;
+	public LayerMask buildButtonMask;
+	public GameObject Tick;
+	private PlaceableBuilding placeAbleBuildingOld;
+	private bool Movingbuilding;
+	private GameObject TickClone;
+	private Touch touch;
+	private CameraControls CamControls;
+	public GameObject groundPlane;
+
+	// Use this for initialization
+	void Start () {
+		Movingbuilding = false;
+		CamControls = GetComponent<CameraControls>();
+		CamControls.SetCameraState (true);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 m = Input.mousePosition;
+		Vector3 pos = new Vector3(0, 20, -8);
+		m = new Vector3(m.x, m.y, transform.position.y);
+
+		Vector3 p = camera.ScreenToWorldPoint(m);
+		Vector3 n = new Vector3 (0.0f, -0.667f, 0.0f);
+
+		if (currentBuilding != null && !hasPlaced) 
+		{
+			if (Input.GetMouseButtonDown(0))
+			{
+				if (TickClone.guiTexture.HitTest(Input.mousePosition)) 	// if user clicks the tick then place the buildin
+				{
+					if (IsLegalPosition())
+					{
+						hasPlaced = true;
+						Movingbuilding = false;
+						Destroy (TickClone);
+					}
+				}
+			}
+			else
+			{			/// allow the player to drag the building around the screen	
+				RaycastHit hit = new RaycastHit();
+				Ray ray = Camera.main.ScreenPointToRay(touch.position);
+				if (Physics.Raycast(ray,out hit, Mathf.Infinity, buildingsMask))	// has hit building
+				{
+					Movingbuilding = true;
+				}
+
+				if ( Input.touchCount > 0)	// if player has touched the screen
+				{
+					touch = Input.touches[0];
+					if (Movingbuilding)
+					{
+						if (Physics.Raycast(ray,out hit, Mathf.Infinity, MotionMask))	// has hit building
+						{
+							CamControls.SetCameraState(false);
+							//CameraStationary = true;
+							switch (touch.phase)
+							{
+							case TouchPhase.Began:
+								Debug.Log("BEGIN MOVEMENT");
+								Movingbuilding = true;
+								break;
+							case TouchPhase.Moved:
+								Vector3 raypoint = ray.GetPoint((hit.distance));
+								currentBuilding.position = new Vector3(raypoint.x, 0 + currentBuilding.localScale.y/2, raypoint.z); // map building to finger position
+								break;
+							case TouchPhase.Stationary:
+								CamControls.SetCameraState(true);
+								Movingbuilding = false;
+								break;						
+							case TouchPhase.Ended:
+								CamControls.SetCameraState(true);
+								Movingbuilding = false;
+								break;
+							case TouchPhase.Canceled:
+								CamControls.SetCameraState(true);
+								break;
+							}
+						}
+					}
+				}
+				else {
+					CamControls.SetCameraState(true);
+				}
+			}
+		}
+		else {
+			if (Input.GetMouseButtonDown(0)) {
+				RaycastHit hit = new RaycastHit();
+				//Ray ray = new Ray(new Vector3(p.x, Camera.main.transform.position.y, p.z), n);
+				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				if (Physics.Raycast(ray,out hit, Mathf.Infinity, buildingsMask)) {		// out sets the orignal value directly
+					if (placeAbleBuildingOld != null) {
+							placeAbleBuildingOld.SetSelected(false);
+					}
+					hit.collider.gameObject.GetComponent<PlaceableBuilding>().SetSelected(true);
+					placeAbleBuildingOld = hit.collider.gameObject.GetComponent<PlaceableBuilding>();
+				}
+				else if (Physics.Raycast(ray,out hit, Mathf.Infinity, MenuMask))
+				{
+					if (placeAbleBuildingOld != null) {
+							placeAbleBuildingOld.SetSelected(false);
+					}
+					Debug.Log("Hit MENU!!");
+					//hit.collider.gameObject.GetComponent<PlaceableBuilding>().SetSelected(true);
+					placeAbleBuildingOld.SetSelected(true);
+				}
+				else
+				{
+					if (placeAbleBuildingOld != null) {
+						placeAbleBuildingOld.SetSelected(false);
+					}
+				}
+			}
+		}
+	}
+
+	bool IsLegalPosition() {
+		if (placeAbleBuilding.colliders.Count > 0) {	// count is the length of list
+			return false;
+		}
+		return true;
+	}
+
+	public void SetItem(GameObject b) {
+		Debug.Log ("Setting the item");
+		hasPlaced = false;
+		currentBuilding = ((GameObject)Instantiate(b)).transform;
+		currentBuilding.name = b.name;
+		placeAbleBuilding = currentBuilding.GetComponent<PlaceableBuilding>();
+		TickClone = (GameObject)Instantiate(Tick);
+	}
+
+	public void MoveBuildings() 
+	{
+		Vector3 m = Input.mousePosition;
+		
+		m = new Vector3(m.x, m.y, transform.position.y);
+		
+		Vector3 p = camera.ScreenToWorldPoint(m);
+		currentBuilding.position = new Vector3(p.x, 0 + currentBuilding.localScale.y/2, p.z); // here change code to make building follow the players finger until another button is pressed to confirm the location?
+		Movingbuilding = true;			
+		Vector3 pos = new Vector3(0, 20, -8);
+		transform.position = pos;
+	}
+}
